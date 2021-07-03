369b3ebc875607d470312a6dc5143c1df463e908
TianZonglin <1805984583@qq.com>
2021-07-04 01:24:43 +0200

    20210704 12443

---
+++ backup/bat.bat
@@ -0,0 +1,15 @@
+echo off & color 0A
+setlocal EnableDelayedExpansion 
+set DIR="%cd%"
+echo DIR=%DIR%
+set /a num = 0
+for /R %DIR% %%f in (*.*) do ( 
+echo %%f
+set /a num += 1
+echo !num!
+xcopy /s %%f E:\tuchuang\backupAllinOne
+)
+pause
+
+::截图
+::clipboard
